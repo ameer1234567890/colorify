e0dabdf357eaed3afa542bcb39291d2a6412f179
Ameer Dawood <ameer1234567890@gmail.com>
2019-08-02 22:18:01 +0500

    New coloring method

--- colorify.user.js
+++ colorify.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         colorify
 // @namespace    https://ameer.io/colorify
-// @version      0.1
+// @version      0.2
 // @description  colorify esp8266 logs
 // @author       Ameer Dawood
 // @match        http://*.lan/log
@@ -14,17 +14,25 @@
     var s = document.createElement("style");
     s.setAttribute("type", "text/css");
     s.appendChild(document.createTextNode("\
-                                          body {font-family: monospace; white-space: pre;} \
-                                          .info {color: green;} \
-                                          .warn {color: orange;} \
-                                          .error {color: red;} \
-                                          .debug {color: blue;} \
+                                          body { \
+                                              font-family: monospace; \
+                                              white-space: pre; \
+                                              color: rgb(187, 187, 187); \
+                                              background-color:rgb(45,15,44); \
+                                          } \
+                                          span {line-height: 18px;} \
+                                          .info {background-color: green;} \
+                                          .warn {background-color: orange;} \
+                                          .error {background-color: red;} \
+                                          .debug {background-color: blue;} \
+                                          .tag {background-color: gray;} \
+                                          .box {color: white; padding: 1px 5px; margin-right: 1px;} \
                                           "));
     document.querySelector("head").appendChild(s);
     var t = document.querySelector("pre").innerText;
-    t = t.replace(new RegExp("(.*) I/([a-z ]{6})(.*)","g"),'<span class="info">$1 I/$2$3</span>');
-    t = t.replace(new RegExp("(.*) E/([a-z ]{6})(.*)","g"),'<span class="error">$1 E/$2$3</span>');
-    t = t.replace(new RegExp("(.*) W/([a-z ]{6})(.*)","g"),'<span class="warn">$1 W/$2$3</span>');
-    t = t.replace(new RegExp("(.*) D/([a-z ]{6})(.*)","g"),'<span class="debug">$1 D/$2$3</span>');
+    t = t.replace(new RegExp("(.*) I/([a-z ]{6})(:)(.*)","g"),'$1 <span class="info box">I</span><span class="tag box">$2</span>$4');
+    t = t.replace(new RegExp("(.*) E/([a-z ]{6})(:)(.*)","g"),'$1 <span class="error box">E</span><span class="tag box">$2</span>$4');
+    t = t.replace(new RegExp("(.*) W/([a-z ]{6})(:)(.*)","g"),'$1 <span class="warn box">W</span><span class="tag box">$2</span>$4');
+    t = t.replace(new RegExp("(.*) D/([a-z ]{6})(:)(.*)","g"),'$1 <span class="debug box">D</span><span class="tag box">$2</span>$4');
     document.querySelector("pre").outerHTML = t;
 })();
